1a8b9e7d5fd71cd37b46ef510530e036e2973a56
Samuel Suesca Ríos <96037106+Samsuesca@users.noreply.github.com>
2024-06-23 17:34:33 -0500

    AVsadasasdadsadadadas

--- paginas/soul_mate.py
+++ paginas/soul_mate.py
@@ -2,10 +2,59 @@ import streamlit as st
 
 st.title("Soul Mate 💖")
 
+# Introducción
 st.write("""
 ¡Feliz cumpleaños a mi querida Soul Mate!
 Aquí tienes un pequeño espacio dedicado a ti para recordarte lo especial que eres.
 """)
 
-st.image("https://source.unsplash.com/400x300/?happy,birthday", caption="¡Feliz Cumpleaños!")
+# Primera imagen y audio
+st.image("images/i1.png", caption="¡Feliz Cumpleaños!")
 st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3", format='audio/mp3')
+
+st.write("--------")
+
+# Texto intercalado
+st.write("""
+### Primer Momento Especial
+Recuerdo aquel día cuando nos conocimos por primera vez. Fue un momento mágico que nunca olvidaré. 
+Tu sonrisa ilumina cualquier lugar y estoy agradecido por cada momento que hemos compartido juntos.
+""")
+
+# Segunda imagen
+st.image("images/i2.png", caption="Momentos Inolvidables")
+
+st.write("--------")
+
+# Texto intercalado
+st.write("""
+### Segundo Momento Especial
+Las aventuras que hemos tenido, desde viajes espontáneos hasta las noches tranquilas en casa, 
+han sido algunas de las mejores experiencias de mi vida. ¡Eres mi compañera en todo!
+""")
+
+# Tercera imagen
+st.image("images/i3.png", caption="Aventuras Juntos")
+
+st.write("--------")
+
+# Texto intercalado
+st.write("""
+### Tercer Momento Especial
+No solo eres mi pareja, sino también mi mejor amiga. Tu apoyo incondicional y amor me han dado la fuerza para ser la mejor versión de mí mismo.
+Gracias por estar siempre a mi lado.
+""")
+
+# Cuarta imagen
+st.image("images/i4.png", caption="Amistad y Amor")
+
+st.write("--------")
+
+# Texto de cierre
+st.write("""
+Cada día a tu lado es un regalo. Espero que este cumpleaños esté lleno de tanta alegría y amor como tú me has dado. 
+¡Te amo con todo mi corazón, y estoy deseando pasar muchos más cumpleaños juntos!
+""")
+
+# Imagen de cierre
+st.image("images/i5.png", caption="Te Amo 💖")
