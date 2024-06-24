f54f5c3aa71d8a6fcf4b347794b967e98602cc6a
Samuel Suesca Ríos <96037106+Samsuesca@users.noreply.github.com>
2024-06-23 22:06:27 -0500

    avance final 643 centerffs

--- app.py
+++ app.py
@@ -60,14 +60,12 @@ st.video("videos/v1.mp4",start_time=7)
 st.write(f"""
 ¡Hola {nombre}!
 
-Este es un regalo especial para ti en tu cumpleaños que hizo tu hermano nerd a no encontrar otra idea. He creado esta página web para celebrar tu día de una manera única y especial, dandole un toque mio jjeejeje. Aquí encontrarás diferentes secciones, cada una con una sorpresa diseñada especialmente para ti. Brevemente te describo cada una de ellas:
+Este es un regalo especial para ti en tu cumpleaños que hizo tu hermano nerd al no encontrar otra idea. He creado esta página web para celebrar tu día de una manera única y especial, dandole un toque mio jjeejeje. Aquí encontrarás diferentes secciones, cada una con una sorpresa diseñada especialmente para ti. Brevemente te describo cada una de ellas:
 
-- **Inicio**: Esta es la presente sección, es la página de bienvenida donde puedes obtener una visión general de todo lo que te espera.
-- **Soul Mate**: Un rincón especial dedicado a nuestra hermandad y a los momentos increíbles que hemos compartido.
-- **Cupones**: Una serie de cupones que puedes canjear por diferentes actividades y regalos a lo largo del año.
-- **Memories**: Una sección dedicada a algunos recuerdos que encontré bien refundidos y quisiera que quedaran en constancia.
+- **[Inicio](#)**: Esta es la presente sección, es la página de bienvenida donde puedes obtener una visión general de todo lo que te espera.
+- **[Soul Mate](?page=soul_mate)**: Un rincón especial dedicado a nuestra hermandad y a los momentos increíbles que hemos compartido.
+- **[Cupones](?page=cupones)**: Una serie de cupones que puedes canjear por diferentes actividades y regalos a lo largo del año.
+- **[Memories](?page=celia)**: Una sección dedicada a algunos recuerdos que encontré bien refundidos y quisiera que quedaran en constancia.
 
 Espero que disfrutes navegando por estas secciones y que cada una de ellas te haga sentir lo especial que eres para mí. ¡Feliz cumpleaños!
 """)
-
-
