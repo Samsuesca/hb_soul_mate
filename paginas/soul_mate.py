b96e4ed087988d0b87a40260b9c85c6769cd40c5
Samuel Suesca Ríos <96037106+Samsuesca@users.noreply.github.com>
2024-06-23 21:56:41 -0500

    avance final 643 center

--- paginas/soul_mate.py
+++ paginas/soul_mate.py
@@ -8,53 +8,55 @@ st.write("""
 Aquí tienes un pequeño espacio dedicado a ti para recordarte lo especial que eres.
 """)
 
+# Función para centrar el contenido
+def center_content(content_func, *args, **kwargs):
+    col1, col2, col3 = st.columns([1, 6, 1])
+    with col2:
+        content_func(*args, **kwargs)
+
 # Primera imagen y audio
-st.image("images/i0.png", caption="¡Feliz Cumpleaños!")
-st.audio("audios/a1.mpeg", format='audio/mp3')
+center_content(st.image, "images/i0.png", caption="¡Feliz Cumpleaños!")
+center_content(st.audio, "audios/a1.mpeg", format='audio/mpeg')
 
 st.write("--------")
 
 # Texto intercalado
 st.write("""
-### Por se quién creció conmigo y me acompaño en tantos momentos.
-Hermanita, siempre recordaré tu compañia desde el dia cero. Vivimos momentos mágicos que nunca olvidaré y que hoy recuerdo de la manera más bella posible.
-Desde las veces en las que jugabamos a que eramos adultos, que teniamos trabajos y que nos invitabamos a nuestros apartamentos (pd: me encanta que la vida nos haya permitido que ya me invites al tuyo jejejee).
-O cuando cantabamos imaginandonos que eramos una banda de rock con pipe y la dabamos toda definitivamente. 
-Es imposible que recuerde todos esos bellos momentos, pero imaginar mi infancia es bonito porque disfruté al lado de alguien increible como tu.
+### Por ser quien creció conmigo y me acompañó en tantos momentos.
+Hermanita, siempre recordaré tu compañía desde el día cero. Vivimos momentos mágicos que nunca olvidaré y que hoy recuerdo de la manera más bella posible.
+Desde las veces en las que jugábamos a que éramos adultos, que teníamos trabajos y que nos invitábamos a nuestros apartamentos (pd: me encanta que la vida nos haya permitido que ya me invites al tuyo jejejee).
+O cuando cantábamos imaginándonos que éramos una banda de rock con Pipe y la dábamos toda definitivamente. 
+Es imposible que recuerde todos esos bellos momentos, pero imaginar mi infancia es bonito porque disfruté al lado de alguien increíble como tú.
 """)
 
-
-st.video("videos/v2.mp4",format="video/mp4")
-
+center_content(st.video, "videos/v2.mp4")
 st.write("Juntos desde chititos")
-# Segunda imagen
 
 st.write("--------")
 
 # Texto intercalado
 st.write("""
 ### My guide.
-Muchas veces fuiste mi guia. A veces reflexiono y concluyo que lo más probable es que fuera autista o un raro que nunca explotó sus habilidades sociales si no me hubieras
-cogido e invitado a tus proyectos. Definitivamente agradezco tanto por hacerme crecer y seguir algunos de tus caminos. En muchos aspectos soy quién soy, gracias a ti.
+Muchas veces fuiste mi guía. A veces reflexiono y concluyo que lo más probable es que hubiera sido un raro que nunca explotó sus habilidades sociales si no me hubieras
+cogido e invitado a tus proyectos. Definitivamente agradezco tanto por hacerme crecer y seguir algunos de tus caminos. En muchos aspectos soy quien soy, gracias a ti.
 """)
 
-# Tercera imagen
-st.image("images/onu.jpeg", caption="Cuando me ayudaste a crecer, antes de crecer")
-st.image("images/med.jpeg", caption="En el proceso mi fai")
-st.image("images/guapos.jpeg", caption="Ahora si, guapos y poderosos")
+# Imágenes intercaladas
+center_content(st.image, "images/onu.jpeg", caption="Cuando me ayudaste a crecer, antes de crecer")
+center_content(st.image, "images/med.jpeg", caption="En el proceso mi fai")
+center_content(st.image, "images/guapos.jpeg", caption="Ahora sí, guapos y poderosos")
 
 st.write("--------")
 
 # Texto intercalado
 st.write("""
-### Mi compañia.
-Dar, definitivamente te agradezco por ser mi compañera de muchos momentos y la hermanita que siempre estuvo ahí para mi. Me encanta tener una hermanita que brille como yo, o que incluso brille mucho más.
-En realidad, nos vemos geniales como hermanos y fuera de charla, aunque fuiste recogida en un basurero, agradezco el dia en que la cucha le dio por hacer semejante acto de caridad.
-JAJAJJAJAA, mentiras, por nada del mundo te cambiaría o negaría a un ser tan espectacular como tu.
+### Mi compañía.
+Dar, definitivamente te agradezco por ser mi compañera de muchos momentos y la hermanita que siempre estuvo ahí para mí. Me encanta tener una hermanita que brille como yo, o que incluso brille mucho más.
+En realidad, nos vemos geniales como hermanos y fuera de charla, aunque fuiste recogida en un basurero, agradezco el día en que la cucha le dio por hacer semejante acto de caridad.
+JAJAJAJAJA, mentiras, por nada del mundo te cambiaría o negaría a un ser tan espectacular como tú.
 """)
 
-# Cuarta imagen
-st.video("videos/v3.mp4")
+center_content(st.video, "videos/v3.mp4")
 st.write("Y faltan muchos más momentos juntos por vivir")
 
 st.write("--------")
@@ -62,16 +64,15 @@ st.write("--------")
 # Texto de cierre
 st.write("""
 My soul mate.
-Herma, tu misma lo haz dicho, tu y yo tenemos una conexión especial. Tal vez sea algo como el ying y el yang o cualquiera de esas mondades, le podemos poner el mistisismo que sea, pero la realidad es esa.
-Nosotros, dentro de nuestro entorno, nuestra familia, y dentro de este extraño universo tenemos una relación especial y que destaca, somos seres radiantes individualmente y juntos. De alguna manera siento que estamos grandes para muy, pero muy grandes cosas, tanto individualmente como juntos.
-Siento que llegarás infinitamente lejos hermanita, tanto como lo siento para mi. 
+Herma, tú misma lo has dicho, tú y yo tenemos una conexión especial. Tal vez sea algo como el ying y el yang o cualquiera de esas mondades, le podemos poner el misticismo que sea, pero la realidad es esa.
+Nosotros, dentro de nuestro entorno, nuestra familia, y dentro de este extraño universo tenemos una relación especial y que destaca, somos seres radiantes individualmente y juntos. De alguna manera siento que estamos destinados a cosas muy, pero muy grandes, tanto individualmente como juntos.
+Siento que llegarás infinitamente lejos hermanita, tanto como lo siento para mí.
 """)
 
 # Imagen de cierre
-st.image("images/tuyyohoy.jpeg", caption="Te Amo Soul Mate")
+center_content(st.image, "images/tuyyohoy.png", caption="Te Amo 💖")
 
 st.write("""
 Mi hermana, llegaremos lejos,
-Eres alguien demasiado radiante y una estrella que en sus fases de vida cada vez brilla más. """)
-
-
+Eres alguien demasiado radiante y una estrella que en sus fases de vida cada vez brilla más.
+""")
